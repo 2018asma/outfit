baea204d2b8057780ce504491d7cb3516dfc3d2b
asma ameen alshanqiti <asmaa.alshanqiti@gmail.com>
2020-06-15 16:34:35 +0300

    carsoul

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, hashHistory} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import "./css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter history={hashHistory} basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,

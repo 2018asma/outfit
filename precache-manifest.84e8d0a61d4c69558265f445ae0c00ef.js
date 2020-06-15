2363890a09b8270619bcaf084f6151c36afc05ce
asma ameen alshanqiti <asmaa.alshanqiti@gmail.com>
2020-06-15 15:02:18 +0300

    Updates

---
+++ precache-manifest.84e8d0a61d4c69558265f445ae0c00ef.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d0c38af79a7882e8cca6aaae82ac71",
+    "url": "/outfit/index.html"
+  },
+  {
+    "revision": "240ed508a471aa6de521",
+    "url": "/outfit/static/css/main.d9aeec0e.chunk.css"
+  },
+  {
+    "revision": "5b3f023a2b7733624138",
+    "url": "/outfit/static/js/2.b2ab9d17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/outfit/static/js/2.b2ab9d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240ed508a471aa6de521",
+    "url": "/outfit/static/js/main.166b8321.chunk.js"
+  },
+  {
+    "revision": "4c6be21e49cd7fa455c8",
+    "url": "/outfit/static/js/runtime-main.9aaa580a.js"
+  },
+  {
+    "revision": "4c369dfeac6b7401e33554b067383678",
+    "url": "/outfit/static/media/1.4c369dfe.webp"
+  },
+  {
+    "revision": "50eb58d1ce6f25c3708094cc05499c38",
+    "url": "/outfit/static/media/1.50eb58d1.webp"
+  },
+  {
+    "revision": "a9a8df20ed20320b3813133198edcbde",
+    "url": "/outfit/static/media/1.a9a8df20.webp"
+  },
+  {
+    "revision": "233ff1785e9569f6c1346d4751d72da1",
+    "url": "/outfit/static/media/10.233ff178.webp"
+  },
+  {
+    "revision": "b38f3318658f97a51bfd56c898023848",
+    "url": "/outfit/static/media/10.b38f3318.webp"
+  },
+  {
+    "revision": "d33fe739548e018341a506d83bcc7d59",
+    "url": "/outfit/static/media/10.d33fe739.webp"
+  },
+  {
+    "revision": "19465f70ec1f3dd014173c8f5df8d5b4",
+    "url": "/outfit/static/media/2.19465f70.webp"
+  },
+  {
+    "revision": "b64a55c6a780c07ceea33e3d38b55231",
+    "url": "/outfit/static/media/2.b64a55c6.webp"
+  },
+  {
+    "revision": "cd507112b1d62db64c09ea3f43b56b4c",
+    "url": "/outfit/static/media/2.cd507112.webp"
+  },
+  {
+    "revision": "4339f1ad2bd7f6ab87821557d00d186c",
+    "url": "/outfit/static/media/3.4339f1ad.webp"
+  },
+  {
+    "revision": "5302b9070d4bf08baddf1d314bf6f383",
+    "url": "/outfit/static/media/3.5302b907.webp"
+  },
+  {
+    "revision": "981a1b67cf0595737a045af41b34b136",
+    "url": "/outfit/static/media/3.981a1b67.webp"
+  },
+  {
+    "revision": "5c170b1d3147e83ea13815bbaf5c5d9b",
+    "url": "/outfit/static/media/4.5c170b1d.webp"
+  },
+  {
+    "revision": "de4a33fa611cfc788b07a80889a123f4",
+    "url": "/outfit/static/media/4.de4a33fa.webp"
+  },
+  {
+    "revision": "f0eb099e817cbc224eddb9b146ba7aae",
+    "url": "/outfit/static/media/4.f0eb099e.webp"
+  },
+  {
+    "revision": "2ef79536f358f68319bf7243eb208b69",
+    "url": "/outfit/static/media/5.2ef79536.webp"
+  },
+  {
+    "revision": "3480216e9453f4497d5a89d9dd09395d",
+    "url": "/outfit/static/media/5.3480216e.webp"
+  },
+  {
+    "revision": "4e293902fb6795beb3f3aac1c72d8eeb",
+    "url": "/outfit/static/media/5.4e293902.webp"
+  },
+  {
+    "revision": "9d508cfdd69b20908a8e654aa6ee487d",
+    "url": "/outfit/static/media/6.9d508cfd.webp"
+  },
+  {
+    "revision": "b4f270b865ac23bbf6095904b393cfe0",
+    "url": "/outfit/static/media/6.b4f270b8.webp"
+  },
+  {
+    "revision": "e08b65c3197acdab15e34857b5fb1c37",
+    "url": "/outfit/static/media/6.e08b65c3.webp"
+  },
+  {
+    "revision": "196022e62d3111f7c985dbc9ee7d4613",
+    "url": "/outfit/static/media/7.196022e6.webp"
+  },
+  {
+    "revision": "e37bd6d7e229180e7a127746ea2daaf0",
+    "url": "/outfit/static/media/7.e37bd6d7.webp"
+  },
+  {
+    "revision": "ff92df06d1eee7e8c7568564c9c83e7b",
+    "url": "/outfit/static/media/7.ff92df06.webp"
+  },
+  {
+    "revision": "1d7859bf71621560488bf1ff56285bbf",
+    "url": "/outfit/static/media/8.1d7859bf.webp"
+  },
+  {
+    "revision": "20fabaf84e97ad15c490ea6930ae3e0b",
+    "url": "/outfit/static/media/8.20fabaf8.webp"
+  },
+  {
+    "revision": "d0a321d2dda8bac063855090db2cb66b",
+    "url": "/outfit/static/media/8.d0a321d2.webp"
+  },
+  {
+    "revision": "043e9198b3b39671551ea7d78ca24b36",
+    "url": "/outfit/static/media/9.043e9198.webp"
+  },
+  {
+    "revision": "10799d2fb406d21d2aea3278e78ccd14",
+    "url": "/outfit/static/media/9.10799d2f.webp"
+  },
+  {
+    "revision": "c9fa52e617a868e21346a1e11685d227",
+    "url": "/outfit/static/media/9.c9fa52e6.webp"
+  },
+  {
+    "revision": "db99d0d1a3bae5ac6cbbea1bc853765b",
+    "url": "/outfit/static/media/kids.db99d0d1.jpg"
+  },
+  {
+    "revision": "f27bf82b932f9cac53074646314add0e",
+    "url": "/outfit/static/media/logo.f27bf82b.svg"
+  },
+  {
+    "revision": "b688b4064594b2726dda442065fc1fef",
+    "url": "/outfit/static/media/man.b688b406.jpeg"
+  },
+  {
+    "revision": "cf3f146566ae11a262f3173a4d74a3ec",
+    "url": "/outfit/static/media/shein-logo.cf3f1465.png"
+  },
+  {
+    "revision": "4d6239b05c1c474a38b03d5158cd526b",
+    "url": "/outfit/static/media/woman.4d6239b0.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/outfit/precache-manifest.84e8d0a61d4c69558265f445ae0c00ef.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/outfit/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
